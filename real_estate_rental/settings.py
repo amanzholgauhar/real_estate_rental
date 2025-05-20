3d281893fe80e3091f5fd721db40c6d824483059
DieeLLlgit config user.email <amanzholgaukhar07@gmail.com>
2025-05-20 18:39:32 +0600

    ci: add postgres service and configure DB via env vars

--- real_estate_rental/settings.py
+++ real_estate_rental/settings.py
@@ -62,13 +62,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "real_estate_rental.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE':   os.getenv('DB_ENGINE',   'django.db.backends.sqlite3'),
+        'NAME':     os.getenv('DB_NAME',     BASE_DIR / 'db.sqlite3'),
+        'USER':     os.getenv('DB_USER',     ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST':     os.getenv('DB_HOST',     ''),
+        'PORT':     os.getenv('DB_PORT',     ''),
     }
 }
 
