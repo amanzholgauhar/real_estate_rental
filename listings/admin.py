65bfaf3e172b4ae18a10cd8cf4d0b33c9cd3bcd7
DieeLLlgit config user.email <amanzholgaukhar07@gmail.com>
2025-05-20 17:30:08 +0600

    chore: apply automatic formatting & lint fixes

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Property, Booking
 
-from .models import Booking
 
 admin.site.register(Booking)
 
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
-
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# …
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
